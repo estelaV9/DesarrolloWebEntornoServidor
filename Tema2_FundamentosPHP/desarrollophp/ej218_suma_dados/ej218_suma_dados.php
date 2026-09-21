618d36390b8fe579d3fe30e29df3add19b15cebf
estelaV9 <esteladevega.dev9@gmail.com>
2026-09-21 20:38:24 +0200

    empezar ejercicio 18 de la suma de dos dados

---
+++ Tema2_FundamentosPHP/desarrollophp/ej218_suma_dados/ej218_suma_dados.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- EJER 18: SUMAR DOS DADOS -->
+    <meta charset="UTF-8">
+    <title>Suma Dado</title>
+</head>
+
+<body>
+    <h1>EJER 18: SUMA DE DADOS</h1>
+
+    <?php
+        $caradado = rand(1, 6);
+        echo "<img src='assets/cara" . $caradado . ".png' width='200px'>";
+    ?>
+</body>
+
+</html>
