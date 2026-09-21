0192f969ee722ba97e54e12298e614d22517e985
estelaV9 <esteladevega.dev9@gmail.com>
2026-09-21 20:45:18 +0200

    terminar ejercicio 8 de la suma de dos dados

--- Tema2_FundamentosPHP/desarrollophp/ej218_suma_dados/ej218_suma_dados.php
+++ Tema2_FundamentosPHP/desarrollophp/ej218_suma_dados/ej218_suma_dados.php
@@ -5,15 +5,29 @@
     <!-- EJER 18: SUMAR DOS DADOS -->
     <meta charset="UTF-8">
     <title>Suma Dado</title>
+    <style>
+        .row {
+            display: flex;
+        }
+    </style>
 </head>
 
 <body>
     <h1>EJER 18: SUMA DE DADOS</h1>
 
-    <?php
-        $caradado = rand(1, 6);
-        echo "<img src='assets/cara" . $caradado . ".png' width='200px'>";
-    ?>
+    <div class="row">
+        <?php
+            $suma = 0;
+            for ($i = 0; $i < 2; $i++) {
+                $caradado = rand(1, 6);
+                $suma += $caradado;
+                echo "<img src='assets/cara" . $caradado . ".png' width='200px'>";
+            }
+        ?>
+    </div>
+
+    <p>TOTAL: <?php echo $suma; ?></p>
+
 </body>
 
 </html>
